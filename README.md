c45dae7bb9f66821f132f53acfa8158ee8ab2270
Jonathan Tinajero <jtinajero@p1stache.com>
2020-02-11 02:01:39 -0600

    First commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Practice from Node course
+
+Testing
---
+++ server/server.js
@@ -0,0 +1,43 @@
+require('config/config');
+
+const express = require('express');
+const app = express();
+
+const bodyParser = require('body-parser');
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+ 
+// parse application/json
+app.use(bodyParser.json())
+ 
+app.get('/usuario', function (req, res) {
+  res.json('Get usuario');
+});
+
+app.post('/usuario', function (req, res) {
+  
+  const body = req.body;
+
+  if (body.nombre === undefined) {
+    res.status(400).json({
+      ok: false,
+      message: "Nombre es requerido"
+    });
+  } else {
+    res.json({ body });
+  }
+});
+
+app.put('/usuario/:id', function (req, res) {
+
+  const id = req.params.id;
+
+  res.json({
+    id
+  });
+});
+ 
+app.listen(process.env.PORT, () => {
+  console.log('Listening 3000 port');
+});
